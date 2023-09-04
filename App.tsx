fbe30e05ba8ec20e9dbfa2e73c0ad443714fe019
Andhika-GIT <andhikapr081@gmail.com>
2023-09-04 17:44:14 +0700

    permission to get user coordinates

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, ImageBackground } from 'react-native';
 
@@ -10,7 +12,31 @@ import Home from './pages/Home/Home';
 // image
 import backgroundImg from './assets/background.png';
 
+// locations
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
 export default function App() {
+  const [coordinates, setCoordinates] = useState<{ lat: string | number; lng: string | number } | null>();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  const getUserCoordinates: () => void = async () => {
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status === 'granted') {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: '48.85', lng: '2.35' });
+    }
+  };
+
+  console.log(coordinates);
   return (
     <ImageBackground imageStyle={s.img} style={s.img_background} source={backgroundImg}>
       <SafeAreaProvider>
